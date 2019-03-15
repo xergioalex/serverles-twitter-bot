64bb9741e2cccb1e1124313c8df23b97cd8c3781
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2019-03-14 21:24:45 -0500

    Logging data

--- src/main.js
+++ src/main.js
@@ -3,25 +3,29 @@ import rp from 'minimal-request-promise'
 import config from './data/config'
 
 const lambdaFunction = async (event, context) => {
+  console.log('---- Begin request ----')
   // Get chucknorris joke
   let chuckNorrisResult = await rp.get('https://api.chucknorris.io/jokes/random')
   let chuckNorrisResultBody = JSON.parse(chuckNorrisResult.body)
 
   if (chuckNorrisResultBody && chuckNorrisResultBody.value) {
+    console.log('---- Chuck Norris API result ----')
+    console.log(chuckNorrisResultBody)
     // Tweet chuck norris joke
     let Twitter = new twit(config)
-    Twitter.post('statuses/update', { status: chuckNorrisResultBody.value }, function(err, data, response) {
-      if (err) {
-        console.log('Something is wrong with Twitter api :(...')
-        console.log(err)
-      }
-      console.log('------- Result ------')
-      console.log(data)
-    })
+    let result = await Twitter.post('statuses/update', { status: chuckNorrisResultBody.value })
+    if (result && result.data) {
+      console.log('---- Twitter result ----')
+      console.log(result.data)
+    } else {
+      console.log('Something is woring with twitter api :(...')
+    }
   } else {
     console.log('Something is woring with chuck norris api :(...')
     console.log(result)
   }
+
+  console.log('---- End request ----')
 }
 
 exports.handler = lambdaFunction
